395b30d12e6ed4ae1c8140b124cef3a1d2243d8e
Ertan Çakır <ertancakiir@gmail.com>
2018-10-20 21:26:02 +0300

    Crop code updated

--- imageCutter.py
+++ imageCutter.py
@@ -13,8 +13,27 @@ class ImageCutter(object):
         ret,thresh = cv2.threshold(im,127,255,0)
         im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-        cv2.imshow("Figure 1",im)
-        cv2.imshow("Figure 2",im2)
+        i = 0
+        for c in contours:
+            c = np.array(c)
+            c = c.reshape(len(c),2)
+            x_max = np.max(c[:,0])
+            x_min = np.min(c[:,0])
+            y_max = np.max(c[:,1])
+            y_min = np.min(c[:,1])
+             
+            #print("{Sol Ust = (" + str(x_min) + "," + str(y_min) + ")")
+            #print("Sag Ust = (" + str(x_max) + "," + str(y_min) + ")")
+            #print("Sol Alt = (" + str(x_min) + "," + str(y_max) + ")")
+            #print("Sag Alt = (" + str(x_max) + "," + str(y_max) + ")}\n")
+             
+            if math.fmod(i,3) == 2:
+                tmpImage = im
+                crop_img = tmpImage[y_min:y_max, x_min:x_max]
+                cv2.imwrite(imageName + str(i) + '.png',crop_img)
+                imageList.append("./" + imageName + str(i) + '.png')
+            i+=1
+        return imageList
 
         
 
