9ecfdb87c46e2b035f3938f83c750571acd617bf
Ertan Çakır <ertancakiir@gmail.com>
2018-10-21 23:29:41 +0300

    Crop Algorithm Changed

--- imageCutter.py
+++ imageCutter.py
@@ -21,18 +21,35 @@ class ImageCutter(object):
             x_min = np.min(c[:,0])
             y_max = np.max(c[:,1])
             y_min = np.min(c[:,1])
+            #I divided it to 7 because there were 7 columns and 7 rows in test image
+            i_width = (x_max - x_min) / 7
+            i_height = (y_max - y_min) / 7
              
             #print("{Sol Ust = (" + str(x_min) + "," + str(y_min) + ")")
             #print("Sag Ust = (" + str(x_max) + "," + str(y_min) + ")")
             #print("Sol Alt = (" + str(x_min) + "," + str(y_max) + ")")
             #print("Sag Alt = (" + str(x_max) + "," + str(y_max) + ")}\n")
-             
-            if math.fmod(i,3) == 2:
-                tmpImage = im
-                crop_img = tmpImage[y_min:y_max, x_min:x_max]
-                cv2.imwrite(imageName + str(i) + '.png',crop_img)
-                imageList.append("./" + imageName + str(i) + '.png')
+            if i == 1:
+                big_image = im[y_min:y_max, x_min:x_max]
+                x_min = 2
+                x_max = i_width
+                y_min = -i_height + 2
+                y_max = 0
+                for k in range(1,50):
+                    temp = big_image
+                    if math.fmod(k,7) == 1:
+                        x_min = 0
+                        x_max = i_width
+                        y_min += i_height
+                        y_max += i_height
+                    cropped_image = temp[y_min:y_max, x_min:x_max]
+                    cv2.imwrite(imageName + str(k) + '.png',cropped_image)
+                    imageList.append(imageName + str(k) + '.png')
+                    x_min += i_width
+                    x_max += i_width
+                break
             i+=1
+
         return imageList
 
         
